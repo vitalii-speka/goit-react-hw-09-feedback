316205dcea976e85bdeb39590f922837d4c7bdef
vitalii-speka <65010140+vitalii-speka@users.noreply.github.com>
2021-03-23 19:48:24 +0200

    +

--- src/App.js
+++ src/App.js
@@ -1,8 +1,76 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
 import Section from './Section';
 import Notification from './Notification';
+
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const option = { good, neutral, bad };
+
+  const onLeaveFeedback = option => {
+    // const name = e.target.name;
+
+    switch (option) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const total = good + neutral + bad;
+
+  // const positivePercent = () => {
+  //   // const total = this.countTotalFeedback();
+  //   // const { good } = this.state;
+
+  //   const percent = Math.round((100 * good) / total);
+
+  //   return percent > 0 ? percent : 0;
+  // };
+  const positivePercent = Math.round((100 * good) / total);
+
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(option)}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+
+      {total === 0 ? (
+        <Notification message="No feedback given"></Notification>
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercent > 0 ? positivePercent : 0}
+          ></Statistics>
+        </Section>
+      )}
+    </>
+  );
+}
+
+/*
 
 class App extends Component {
   state = {
@@ -13,20 +81,11 @@ class App extends Component {
 
   onLeaveFeedback = e => {
     const name = e.target.name;
-    this.setState(prevStata => ({
-      [name]: prevStata[name] + 1,
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
     }));
   };
 
-  /* вариант 1 - не очень удачный (Ментор завернул....)
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-
-    return total;
-  };
-  */
-  /* Вариант Метора */
   countTotalFeedback = () =>
     Object.values(this.state).reduce((acc, value) => acc + value, 0);
 
@@ -74,3 +133,4 @@ class App extends Component {
 }
 
 export default App;
+*/
